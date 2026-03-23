e77071c8ca44c9658aa6556a78c7262b7f3fc5d8
Evvard <ewan.olivierverdier1@gmail.com>
2026-03-23 16:48:29 +0100

    message

--- Day07/ex2/EliteCard.py
+++ Day07/ex2/EliteCard.py
@@ -5,9 +5,89 @@ from ex2.Magical import Magical
 
 class EliteCard (Card, Combatable, Magical):
 
+    def __init__(self) -> None:
+        pass
+
     def play(self, game_state: dict) -> dict:
+        try:
+            self.name = str(game_state['name'])
+            self.combat_type = str(game_state['combat_type'])
+            self.damage = int(game_state['damage'])
+            self.mana = int(game_state['mana_used'])
+        except Exception:
+            print('Wrong Value in iput Stop the Program')
+            return {}
+        finally:
+            print(f'Playing {self.name} (Elite Card)')
+            res = {'card_played': self.name,
+                   'mana used': self.mana,
+                   'effect': 'Creature summoned to battlefield'}
+            return {res}
 
-    def attack(self, target) -> dict:
+    def get_card_info(self) -> dict:
+        res = {'name': self.name, 'combat_type': self.combat_type,
+               'damage': self.damage, 'mana': self.mana}
+        return res
 
+    def is_playable(self, available_mana: int) -> bool:
+        if self.mana < available_mana:
+            return False
+        return True
+
+    def attack(self, target: str) -> dict:
+        try:
+            str(target)
+        except TypeError:
+            print('Please Enter valid \'taget\' like in string format')
+            return {}
+        finally:
+            return {'attacker': self.name, 'target': target,
+                    'damage': self.damage, 'combat_type': self.combat_type}
+
+    def defend(self, incoming_damage: int) -> dict:
+        try:
+            int(incoming_damage)
+        except TypeError:
+            print('Please Enter valid \'incoming_damage\' like in int format')
+            return {}
+        finally:
+            if (self.life - incoming_damage) > 0:
+                still_alive = True
+            else:
+                still_alive = False
+            return {'defender': self.name, 'damage_taken': incoming_damage,
+                    'damage_blocked': (self.damage - incoming_damage),
+                    'still_alive': still_alive}
+
+    def get_combat_stats(self) -> dict:
+        return {'Action': 'Combat phase:'}
 
     def cast_spell(self, spell_name: str, targets: list) -> dict:
+        try:
+            str(spell_name)
+            list(targets)
+        except TypeError:
+            m = 'and \'target\' in list format !'
+            print('Please Enter valid \'spell_name\' like in string format', m)
+            return {}
+        finally:
+            return {'caster': self.name, 'spell': spell_name,
+                    'targets': targets, 'mana_used': self.mana}
+
+    def channel_mana(self, amount: int = None) -> dict:
+        try:
+            if (amount <= 0) or not amount:
+                raise IndexError
+            elif (amount < self.mana):
+                raise ValueError
+        except ValueError:
+            print('You cheat bro, you use ability with not enought mana !')
+        except IndexError:
+            print('Negative value not accept !')
+            return {}
+        finally:
+            channeled = amount - self.mana
+            return {'channeled': channeled, 'total_mana': amount}
+
+    def get_magic_stats(self) -> dict:
+        return {'Action': 'Magic phase:'}
--- Day07/ex2/main.py
+++ Day07/ex2/main.py
@@ -1,7 +1,27 @@
+from ex2.EliteCard import EliteCard
+
+
+def main() -> None:
+    print('\n=== DataDeck Ability System ===\n')
+
+    print('EliteCard capabilities:')
+    print('- Card: [\'play\', \'get_card_info\', \'is_playable\']')
+    print('- Combatable: [\'attack\', \'defend\', \'get_combat_stats\']')
+    print('- Magical: [\'cast_spell\', \'channel_mana\', \'get_magic_stats\']')
+    print()
+
+    data = {'name': 'Arcane Warrior', 'combat_type': 'melee', 'damage': 5,
+            'mana_used': 4}
+    elite_card = EliteCard()
+    play = elite_card.play(data)
+    if play == {}:
+        return
+    elite_card.get_combat_stats()
 
 
 
 
 
 
-print('\n=== DataDeck Ability System ===\n')
+if __name__ == '__main__':
+    main()
