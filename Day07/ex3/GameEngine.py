45ad1351fee595eef21aa14bc2cb15041a354cac
eolivier <ewan.olivierverdier1@gmail.com>
2026-03-24 20:32:18 +0100

    ex3_non_fini

--- Day07/ex3/GameEngine.py
+++ Day07/ex3/GameEngine.py
@@ -4,7 +4,9 @@ from ex3.GameStrategy import GameStrategy
 
 class GameEngine:
     def __init__(self):
-        self.strategie = None
+        self.strategie_name = None
+        self._factory = None
+        self._strategy = None
         self.turn = 0
         self.damage = 0
         self.name = 0
@@ -13,23 +15,27 @@ class GameEngine:
                          strategy: GameStrategy) -> None:
         if not isinstance(factory, CardFactory):
             return
-        elif not isinstance(strategy, GameStrategy):
+        if not isinstance(strategy, GameStrategy):
             return
-        self.strategie = strategy.get_strategy_name()
+        self.strategie_name = strategy.get_strategy_name()
         self._factory = factory
         self._strategy = strategy
 
     def simulate_turn(self) -> dict:
-        ww = self.strategie.execute_turn([], ['Enemy Player'])
-        self.name = len(ww['cards_played'])
+        deck = self._factory.create_themed_deck(3)
+        hand = deck.get('creatures', []) + deck.get('spells', [])
+        hand += deck.get('artifacts', [])
+
+        ww = self._strategy.execute_turn(hand, ['Enemy Player'])
+        self.name += len(ww['cards_played'])
         self.damage += int(ww['damage_dealt'])
         self.turn += 1
-        return {}
+        return ww
 
     def get_engine_status(self) -> dict:
         return {
                 'turns_simulated': self.turn,
-                'strategy_used': self.strategie,
+                'strategy_used': self.strategie_name,
                 'total_damage': self.damage,
                 'cards_created': self.name
                 }
--- Day07/ex3/main.py
+++ Day07/ex3/main.py
@@ -5,79 +5,39 @@ from ex0.CreatureCard import CreatureCard
 from ex1.main import SpellCard
 
 
-
-
-
 def main() -> None:
 
-print("\n=== DataDeck Game Engine ===\n")
+    print("\n=== DataDeck Game Engine ===\n")
+    print("Configuring Fantasy Card Game...")
+    factory = FantasyCardFactory()
+    strategy = AggressiveStrategy()
+    engine = GameEngine()
+    print(f"Factory: {factory.__class__.__name__}")
+    print(f"Strategy: {strategy.get_strategy_name()}")
+    supported_types = {'creatures': ['dragon', 'goblin'],
+                       'spells': ['fireball'],
+                       'artifacts': ['mana_ring']}
+    print(f"Available types: {supported_types}\n")
+    hand = [
+            CreatureCard('Fire Dragon', 5, 'Common', 5, 10),
+            CreatureCard('Goblin Warrior', 2, 'Common', 3, 8),
+            SpellCard('Lightning Bolt', 3, 'Common', 'Deal 3 damage')
+            ]
+    print("Simulating aggressive turn...")
+    print(f"Hand: {[f'{card.name} ({card.cost})' for card in hand]}")
+    engine.configure_engine(factory, strategy)
 
-print("Configuring Fantasy Card Game...")
+    turn_result = engine.simulate_turn()
 
-
-
-factory = FantasyCardFactory()
-
-strategy = AggressiveStrategy()
-
-
-
-engine = GameEngine()
-
-print(f"Factory: {factory.__class__.__name__}")
-
-print(f"Strategy: {strategy.get_strategy_name()}")
-
-
-
-supported_types = {'creatures': ['dragon', 'goblin'],
-
-'spells': ['fireball'],
-
-'artifacts': ['mana_ring']}
-
-print(f"Available types: {supported_types}\n")
-
-
-
-hand = [
-
-CreatureCard('Fire Dragon', 5, 'Common', 5, 10),
-
-CreatureCard('Goblin Warrior', 2, 'Common', 3, 8),
-
-SpellCard('Lightning Bolt', 3, 'Common', 'Deal 3 damage')
-
-]
+    print("\nTurn execution:")
+    print(f"Strategy: {strategy.get_strategy_name()}")
+    print(f"Actions: {turn_result}")
+    status = engine.get_engine_status()
+    print("\nGame Report:")
+    print(status)
+    message = 'Abstract Factory'
+    print(f"\n{message} + Strategy Pattern: Maximum flexibility achieved!")
 
 
-
-print("Simulating aggressive turn...")
-
-print(f"Hand: {[f'{card.name} ({card.cost})' for card in hand]}")
-
-
-
-turn_result = strategy.execute_turn(hand, ['Enemy Player'])
-
-print("\nTurn execution:")
-
-print(f"Strategy: {strategy.get_strategy_name()}")
-
-print(f"Actions: {turn_result}")
-
-
-
-engine.configure_engine(factory, strategy)
-
-status = engine.get_engine_status()
-
-
-
-print("\nGame Report:")
-
-print(status)
-
-message = 'Abstract Factory'
-
-print(f"\n{message} + Strategy Pattern: Maximum flexibility achieved!")
+if __name__ == "__main__":
+    main()
