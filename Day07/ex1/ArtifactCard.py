6966ba30a61b7e84abe81425932b7a10cc0a1046
Evvard <ewan.olivierverdier1@gmail.com>
2026-03-30 13:05:07 +0200

    r

--- Day07/ex1/ArtifactCard.py
+++ Day07/ex1/ArtifactCard.py
@@ -4,7 +4,7 @@ from ex0.Card import Card
 class ArtifactCard(Card):
 
     def __init__(self, name: str, cost: int, rarity: str = 'Common',
-                 durability: int = '1', effect: str = 'give mana'):
+                 durability: int = 1, effect: str = 'give mana'):
         super().__init__(name, cost, rarity)
         if durability == 9991776:
             self.durability = 'Permanent'
@@ -12,11 +12,11 @@ class ArtifactCard(Card):
             self.durability = durability
         self.effect = effect
 
-    def play(self, game_state: dict = None) -> dict:
+    def play(self, game_state: dict = {}) -> dict:
         message = f'{self.durability}: {self.effect}'
         play_result = {'card_played': self.name, 'mana_used': self.cost,
                        'effect': message}
         return play_result
 
     def activate_ability(self) -> dict:
-        pass
+        return {}
--- Day07/ex1/SpellCard.py
+++ Day07/ex1/SpellCard.py
@@ -8,10 +8,10 @@ class SpellCard(Card):
         super().__init__(name, cost, rarity)
         self.effect_type = effect_type
 
-    def play(self, game_state: dict = None) -> dict:
+    def play(self, game_state: dict = {}) -> dict:
         play_result = {'card_played': self.name, 'mana_used': self.cost,
                        'effect': self.effect_type}
         return play_result
 
     def resolve_effect(self, targets: list) -> dict:
-        pass
+        return {}
