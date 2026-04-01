2330e18892aabaa46952cf2eff24e36da8f3f47c
Evvard <ewan.olivierverdier1@gmail.com>
2026-04-01 22:12:01 +0200

    day09_finish

---
+++ Day10/ex0/lambda_spells.py
@@ -0,0 +1,41 @@
+from typing import List
+
+
+
+map, filter, sorted, print()
+
+
+
+
+
+
+
+
+def artifact_sorter(artifacts: List[dict]) -> List[dict]:
+
+
+
+
+def power_filter(mages: List[dict], min_power: int) -> List[dict]:
+
+
+
+
+def spell_transformer(spells: List[str]) -> List[str]:
+
+
+
+def mage_stats(mages: List[dict]) -> dict:
+
+
+
+def main() -> None:
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    main()
