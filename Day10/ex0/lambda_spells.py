87f7c9e82088c7c3fe56e5bfe1a5694347d0a3f5
eolivier <ewan.olivierverdier1@gmail.com>
2026-04-02 18:23:25 +0200

    test

--- Day10/ex0/lambda_spells.py
+++ Day10/ex0/lambda_spells.py
@@ -1,36 +1,35 @@
 from typing import List
 
 
-
-map, filter, sorted, print()
-
-
-
-
-
-
-
-
 def artifact_sorter(artifacts: List[dict]) -> List[dict]:
-
-
+    return sorted(artifacts, key=lambda items: items["power"], reverse=True)
 
 
 def power_filter(mages: List[dict], min_power: int) -> List[dict]:
-
-
+    return filter(lambda item: item['power'] >= min_power, mages)
 
 
 def spell_transformer(spells: List[str]) -> List[str]:
+    return map(lambda nv: f"*{nv}*", spells)
 
 
 
-def mage_stats(mages: List[dict]) -> dict:
+def mage_stats(mages: list[dict]) -> dict:
 
 
 
 def main() -> None:
+    dictionnary = [{'name': "test", 'power': 3, 'element': "fire"},
+                   {'name': "new", 'power': 55, 'element': "ice"},
+                   {'name': "fires", 'power': 1, 'element': "sun"}]
+    print()
+    print("Resultat de arrtifact_sorter :", artifact_sorter(dictionnary))
+    print()
+    print("resultat de power_filter :", list(power_filter(dictionnary, 2)))
 
+    spell = ["Fire", "Ice", "92i"]
+    print()
+    print(list(spell_transformer(spell)))
 
 
 
